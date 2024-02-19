a8c8782a650cac7886b2341530e147a40f002ffd
sulemanshaik <sulemanshaik109@gmail.com>
2024-02-19 16:09:43 +0000

    Bookshelves Done

---
+++ src/components/BookDetails/index.js
@@ -0,0 +1,15 @@
+import {Component} from 'react'
+
+import Header from '../Header'
+
+class BookDetails extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    )
+  }
+}
+
+export default BookDetails
